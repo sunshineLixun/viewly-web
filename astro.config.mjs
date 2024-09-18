2a3ec70b7e213b69c46cd2af0de5975a89f63bbf
lixun <lixunemail@gmail.com>
2024-09-18 14:39:56 +0800

    fix: bugfix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://nebulla.vercel.app",
+  site: "https://viewly-web.vercel.app",
   integrations: [tailwind(), react(), mdx(), sitemap()],
 });
